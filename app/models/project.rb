a586f0775a58dfbc8d36cf43a6c4be841f1bf332
Joshua Briley <josh@thebrileys.com>
2021-10-25 16:54:45 -0400

    Add previous and next nav to projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  def self.get_previous_project(current_project)
+    Project.where("projects.id < ? ", current_project.id).order('created_at asc').last
+  end
+  def self.get_next_project(current_project)
+    Project.where("projects.id > ? ", current_project.id).order('created_at asc').first
+  end
 end
