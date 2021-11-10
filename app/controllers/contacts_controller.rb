118dedcedec1043efa7fc1565b5ed2aea35757ef
Joshua Briley <josh@thebrileys.com>
2021-11-10 15:44:02 -0500

    Add authenticity token.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :verify_authenticity_token
   def new
     @contact = Contact.new
   end
